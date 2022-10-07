7461f27ee3232ee2942a3f286d187ef208835fd0
DIogo Alpino <diogoalpino@gmail.com>
2022-10-07 16:58:10 -0300

    Requsito 2 + state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pesquisa: '',
+    };
+  }
+
+  handleSearch = ({ target }) => {
+    const { value } = target;
+    this.setState({ pesquisa: value });
+  };
+
   render() {
+    const { pesquisa } = this.state;
     return (
-      <p
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <>
+        <input type="text" value={ pesquisa } onChange={ this.handleSearch } />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </>
     );
   }
 }
