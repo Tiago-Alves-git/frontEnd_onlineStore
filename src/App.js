557cc12776fe27485609f9c890c71f51bb1decc0
wanderson-rpf <wandersonrpf@gmail.com>
2022-10-14 19:57:36 -0300

    :recycle: [refactor] Apagando comentarios e logs

--- src/App.js
+++ src/App.js
@@ -18,15 +18,6 @@ class App extends React.Component {
     this.setState({ addToCart: cartLocalStorage });
   };
 
-  // amountItemsCart = () => {
-  //   const { addToCart } = this.state;
-  //   const totalProd = addToCart.reduce((count, prod) => {
-  //     count += prod.quantidade;
-  //     return count;
-  //   }, 0);
-  //   localStorage.setItem('amountItemsCart', totalProd);
-  // };
-
   handleButton = (param) => {
     param.quantidade = 1;
     this.setState((prevState) => ({
