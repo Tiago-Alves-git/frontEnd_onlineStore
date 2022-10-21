dc8264a88c5b4c7410d8864133613fc1ebd5cdfa
wanderson-rpf <wandersonrpf@gmail.com>
2022-10-21 18:02:40 -0300

    :lipstick: [feat] Estilizando paginas de detalhes

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,10 +1,13 @@
+/* eslint-disable react/jsx-max-depth */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { MdOutlineLocalShipping } from 'react-icons/md';
+import { BsFillCartPlusFill } from 'react-icons/bs';
 import { getProductById } from '../services/api';
 import Formulario from '../components/Formulario';
 import Header from '../components/Header';
+import '../style/ProductDetails.css';
 
 class ProductDetails extends React.Component {
   constructor() {
@@ -22,14 +25,14 @@ class ProductDetails extends React.Component {
     this.setState({ produtos: response, loading: true });
   }
 
-  countItemsCart = () => {
-    const { addToCart } = this.props;
-    const totalProd = addToCart?.reduce((count, prod) => {
-      count += prod.quantidade;
-      return count;
-    }, 0);
-    return totalProd;
-  };
+  // countItemsCart = () => {
+  //   const { addToCart } = this.props;
+  //   const totalProd = addToCart?.reduce((count, prod) => {
+  //     count += prod.quantidade;
+  //     return count;
+  //   }, 0);
+  //   return totalProd;
+  // };
 
   render() {
     const { produtos, loading } = this.state;
@@ -41,45 +44,56 @@ class ProductDetails extends React.Component {
           addToCart={ addToCart }
           getCart={ getCart }
         />
-        <Link to="/cart">
+        {/* <Link to="/cart">
           <button
             data-testid="shopping-cart-button"
             type="button"
           >
             Carrinho de Compras
           </button>
-        </Link>
-        <p data-testid="shopping-cart-size">
+        </Link> */}
+        {/* <p data-testid="shopping-cart-size">
           { this.countItemsCart() }
-        </p>
-        <div key={ produtos.id }>
-          <h1 data-testid="product-detail-name">{produtos.title}</h1>
-          <img
-            src={ produtos.thumbnail }
-            alt={ produtos.title }
-            data-testid="product-detail-image"
-          />
-          {console.log(produtos)}
-          { loading && produtos?.shipping.free_shipping
-                  && (
-                    <p data-testid="free-shipping">
-                      <MdOutlineLocalShipping />
-                      {' '}
-                      Frete Gratis
-                    </p>
-                  )}
-          <p data-testid="product-detail-price">{produtos.price}</p>
+        </p> */}
+        <div key={ produtos.id } className="container-product-detail">
+          <div className="product-details">
+            <img
+              src={ produtos.thumbnail }
+              alt={ produtos.title }
+              data-testid="product-detail-image"
+              className="product-img"
+            />
+            <div className="container-price">
+              <h2 data-testid="product-detail-name">{produtos.title}</h2>
+              {/* {console.log(produtos)} */}
+              { loading && produtos?.shipping.free_shipping
+                    && (
+                      <p data-testid="free-shipping">
+                        <MdOutlineLocalShipping />
+                        {' '}
+                        Frete Gratis
+                      </p>
+                    )}
+              <p data-testid="product-detail-price" className="price">
+                {`R$${Number(produtos.price).toFixed(2)}`}
+              </p>
+              <button
+                data-testid="product-detail-add-to-cart"
+                type="button"
+                onClick={ () => handleButton(produtos) }
+                className="cssbuttons-io-button"
+              >
+                Adicionar ao carrinho
+                <div className="icon">
+                  <BsFillCartPlusFill className="icon-add-to-cart" />
+                </div>
+              </button>
+            </div>
+          </div>
         </div>
-
-        {loading && <Formulario idProduto={ produtos.id } /> }
-
-        <button
-          data-testid="product-detail-add-to-cart"
-          type="button"
-          onClick={ () => handleButton(produtos) }
-        >
-          Adicionar ao carrinho
-        </button>
+        <div className="container-form">
+          {loading && <Formulario idProduto={ produtos.id } /> }
+        </div>
       </>
     );
   }
